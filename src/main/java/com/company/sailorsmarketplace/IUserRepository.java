641452545996a888151c9f0d453116a542be3ee8
Mackevaki <sshohus@gmail.com>
2018-08-24 20:59:30 +0300

    added bd

--- src/main/java/com/company/sailorsmarketplace/IUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.company.sailorsmarketplace;
-
-import com.company.sailorsmarketplace.dbmodel.AccountEntity;
-
-public interface UserRepository {
-    AccountEntity findByEmail(String email);
-    AccountEntity findByConfirmationToken(String confirmationToken);
-}
